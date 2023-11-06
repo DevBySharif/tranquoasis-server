1338630e21802d1fb9e6d735e158e9248e65d13a
Sharif Obaidullah <sharifmdobaidullah@gmail.com>
2023-11-06 23:32:03 +0600

    DELETE operation for deleting service

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
         const result =await serviceCollection.updateOne(filter,updatedService,options)
         res.send(result)
       })
+
+      app.delete('/api/v1/services/:id',async(req,res)=>{
+        const id = req.params.id
+        const query = {_id:new ObjectId(id)}
+        const result = await serviceCollection.deleteOne(query)
+        res.send(result)
+      })
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
